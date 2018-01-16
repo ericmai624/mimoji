63ecaf32e858b0c0a3f3c99cc3adf149163dda65
eric mai <enmai1988@gmail.com>
2018-01-16 02:11:50 -0800

    removed playbackConfig option

--- src/cast.js
+++ src/cast.js
@@ -1,8 +1,34 @@
 const context = cast.framework.CastReceiverContext.getInstance();
-const playerManager = context.getPlayerManager();
-  
+
+/* Config for playback */
 const playbackConfig = new cast.framework.PlaybackConfig();
 playbackConfig.initialBandwidth = 10 * 1024 * 1024;
 
-context.setLoggerLevel(cast.framework.LoggerLevel.NONE);
+/* Set up the player to handle messages and events */
+const playerManager = context.getPlayerManager();
+
+playerManager.addEventListener(cast.framework.events.EventType.PLAYER_LOAD_COMPLETE, () => {
+  context.sendCustomMessage('urn:x-cast:playermanager.ready', undefined, 'Player is ready');
+});
+
+/* Customer messages */
+const ADD_TEXTTRACK = 'urn:x-cast:texttrack.add';
+context.addCustomMessageListener(ADD_TEXTTRACK, (evt) => {
+  // Create text tracks object
+  const textTracksManager = playerManager.getTextTracksManager();
+  const track = textTracksManager.createTrack();
+  track.trackId = Math.round(Math.random() * 100);
+  track.trackContentType = 'text/vtt';
+  track.trackContentId = evt.data.url;
+  track.language = 'zh';
+
+  // Add tracks
+  textTracksManager.addTracks([track]);
+
+  // Activate the track
+  textTracksManager.setActiveByIds([track.trackId]);
+});
+
+/* Start the receiver app */
+// context.setLoggerLevel(cast.framework.LoggerLevel.NONE);
 context.start({ playbackConfig });
