c4ce9ddf5478b1aaca67c6e5ae8b5cd2ee187752
Eric Mai <enmai1988@gmail.com>
2018-02-28 10:27:35 -0800

    minor update

--- src/cast.js
+++ src/cast.js
@@ -7,19 +7,15 @@ playbackConfig.initialBandwidth = 10 * 1024 * 1024;
 /* Set up the player to handle messages and events */
 const playerManager = context.getPlayerManager();
 
-playerManager.addEventListener(cast.framework.events.EventType.PLAYER_LOAD_COMPLETE, () => {
-  context.sendCustomMessage('urn:x-cast:playermanager.ready', undefined, 'Player is ready');
-});
-
 /* Customer messages */
 const ADD_TEXTTRACK = 'urn:x-cast:texttrack.add';
-context.addCustomMessageListener(ADD_TEXTTRACK, (evt) => {
+const handleAddTextTrackMessage = e => {
   // Create text tracks object
   const textTracksManager = playerManager.getTextTracksManager();
   const track = textTracksManager.createTrack();
   track.trackId = Math.round(Math.random() * 100);
   track.trackContentType = 'text/vtt';
-  track.trackContentId = evt.data.url;
+  track.trackContentId = e.data.url;
   track.language = 'zh';
 
   // Add tracks
@@ -27,7 +23,9 @@ context.addCustomMessageListener(ADD_TEXTTRACK, (evt) => {
 
   // Activate the track
   textTracksManager.setActiveByIds([track.trackId]);
-});
+};
+
+context.addCustomMessageListener(ADD_TEXTTRACK, handleAddTextTrackMessage);
 
 /* Start the receiver app */
 // context.setLoggerLevel(cast.framework.LoggerLevel.NONE);
